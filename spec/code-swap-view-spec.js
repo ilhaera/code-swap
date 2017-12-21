a26b60a0cf8610445252978255f07b786bfc151d
ilhaera <ilhaera@gmail.com>
2017-12-21 21:22:19 +0900

    1.0.0

---
+++ spec/code-swap-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodeSwapView from '../lib/code-swap-view';
+
+describe('CodeSwapView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
